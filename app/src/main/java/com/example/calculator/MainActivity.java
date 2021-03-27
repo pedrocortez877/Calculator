e2ce10d579976c71d4136b46c060d9f6823b8cbd
Pedro Cortez <pedrocortez877@gmail.com>
2021-03-27 18:35:27 -0300

    Alteração no tema, adição de margin nos botões e inicio da funcionalidade dos botões

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean modeResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.modeResult = true;
+    }
+
+    public void onClickButtonNumbers(View view){
+        Button button = (Button) view;
+        String buttonValue = button.getText().toString();
+        TextView txtDisplay = findViewById(R.id.txtDisplay);
+        if(this.modeResult){
+            txtDisplay.setText(buttonValue);
+            this.modeResult = false;
+        }else{
+            txtDisplay.append(buttonValue);
+        }
+    }
+
+    public void onClickButtonOperation(View view){
+        Button button = (Button) view;
+        String buttonValue = button.getText().toString();
+        TextView txtDisplay = findViewById(R.id.txtDisplay);
+        if(this.modeResult){
+            txtDisplay.setText(buttonValue);
+            this.modeResult = false;
+        }else{
+            txtDisplay.append(buttonValue);
+        }
     }
 }
